c7b1314b5ec22dae9a7b18db227f999ed661fe36
Greg Robleto <greg@robleto.com>
2025-05-23 19:00:47 -0400

    fix: correct padding classes in main element for consistent layout

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<Header />
-			<main className="flex flex-col items-center justify-center min-h-screen bg-gray-200 p-6">
+			<main className="flex flex-col items-center justify-center min-h-screen px-6 py-6 bg-gray-200">
 				<Grid />
 			</main>
 			<Footer />
